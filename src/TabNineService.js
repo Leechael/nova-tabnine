038bd50f57497126426a80ff949dd1eae6de3d1a
Leechael Yim <yanleech@gmail.com>
2021-12-02 16:52:36 +0800

    imp: adjust argument based on TabNine for vscode

--- src/TabNineService.js
+++ src/TabNineService.js
@@ -39,7 +39,7 @@ class TabNineService {
     if (binaryPath) {
       this.process = new Process(binaryPath, {
         stdio: 'pipe',
-        args: ["--client=nova"],
+        args: ["--client=nova", "--no-lsp=true"],
       })
       this.reader = this.process.onStdout(this.onStdout, this)
       this.writer = this.process.stdin.getWriter()
